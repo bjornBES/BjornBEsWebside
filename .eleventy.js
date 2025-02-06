752a55677e3d3b635f87ac816906c388245f0e2e
bjornBEs <bjorn@r66.dk>
2025-02-06 13:09:12 +0100

    worked on the layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 
+const {DateTime} = require("luxon")
+
 module.exports = function(eleventyConfig)
 {
     eleventyConfig.addPassthroughCopy('./src/style')
     eleventyConfig.addPassthroughCopy('./src/js')
 
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+    
     return {
         dir: {
             input: "src",
---
+++ src/blog/2025-02-07 Test-Blog copy.md
@@ -0,0 +1,9 @@
+---
+title: First blog
+description: hello world my name is bjornBEs and i have just made this blog
+  where i will show me progress and show new projects i am working on...
+author: BjornBEs
+date: 2025-02-06T18:54:00.000Z
+tags:
+  - post
+---
